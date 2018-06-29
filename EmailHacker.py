ce29fa2fbce4a867a2d030221ac3565749ead35d
Saintmalik <37118134+saintmalik@users.noreply.github.com>
2018-06-29 05:54:29 +0100

    Update EmailHacker.py

--- EmailHacker.py
+++ EmailHacker.py
@@ -70,7 +70,7 @@ print
 ehacker = raw_input ("[*] E-hacker > " )
 
 if ehacker == '01' or ehacker == '1' :
-print
+   
 print '          +---------------------------+'
 print '          |     Cisco Brute Force     |'
 print '          +---------------------------+'
